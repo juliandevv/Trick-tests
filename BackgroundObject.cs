ed6f98d021f800d6a541072e0d5aee8d7ec67182
Julian DeVries <devrjuli@gmail.com>
2021-12-22 07:42:53 -0500

    integrated background objects and restructured update/draw loops

---
+++ BackgroundObject.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trick_tests
+{
+    class BackgroundObject
+    {
+        private Texture2D _texture;
+        private Rectangle _rectangle;
+        private Vector2 _speed;
+        private Vector2 _location;
+       
+
+        public BackgroundObject(Texture2D texture, Rectangle rect, Vector2 speed)
+        {
+            _texture = texture;
+            _rectangle = rect;
+            _speed = speed;
+            _location = new Vector2(rect.X, rect.Y);
+        }
+
+        public Texture2D Texture
+        {
+            get { return _texture; }
+        }
+
+        public Rectangle Bounds
+        {
+            get { return _rectangle; }
+            set { _rectangle = value;
+                _location = new Vector2(value.X, value.Y); }
+        }
+        
+        public Vector2 Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        //public Point Location
+        //{
+        //    get { return _location; }
+        //    set { _location = value; }
+        //}
+
+        //public BackgroundObject CompareTo(BackgroundObject compareSpeed)
+        //{
+        //    return this.Speed.X.CompareTo(compareSpeed.Speed.X);
+        //}
+
+        //public void Reset(GraphicsDeviceManager _graphics)
+        //{
+        //    _location.X = genertaor.Next(_graphics.PreferredBackBufferWidth - 100, _graphics.PreferredBackBufferWidth);
+        //    _rectangle = new Rectangle((int)_location.X, (int)_location.Y, _rectangle.Width, _rectangle.Height);
+        //}
+
+        public void Move()
+        {
+     
+            _location.X += _speed.X;
+            _rectangle.Location = new Point(_location.ToPoint().X, _location.ToPoint().Y);
+
+        }
+
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(this.Texture, this.Bounds, Color.White);
+        }
+    }
+
+}
--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 
 namespace Trick_tests
@@ -10,13 +11,21 @@ namespace Trick_tests
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        //skater
         List<Texture2D> skaterTextures = new List<Texture2D>();
         List<Texture2D> boardTextures = new List<Texture2D>();
         Skater skater;
         KeyboardState keyboardState;
         float jumpStartTime;
         bool jumpKeyPressed;
-        
+
+        //background
+        List<BackgroundObject> backgroundObjects = new List<BackgroundObject>();
+        List<Texture2D> backgroundTextures = new List<Texture2D>();
+        Vector2 speedLevel1, speedLevel2, speedLevel3;
+        Random generator;
+
+
         public enum Trick
         {
             None,
@@ -27,7 +36,14 @@ namespace Trick_tests
 
         }
 
+        public enum Screen
+        {
+            Title,
+            MainGame
+        }
+
         Trick trick;
+        Screen currentScreen;
 
         public Game1()
         {
@@ -38,27 +54,49 @@ namespace Trick_tests
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+
+            _graphics.PreferredBackBufferWidth = 1200;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
+
+            speedLevel1 = new Vector2(-2f, 0);
+            speedLevel2 = new Vector2(-1f, 0);
+            speedLevel3 = new Vector2(-0.5f, 0);
+
+            generator = new Random();
 
             base.Initialize();
 
+            //initial screen
+            currentScreen = Screen.Title;
+
+            //background
+            backgroundObjects.Add(new BackgroundObject(backgroundTextures[0], new Rectangle(200, 200, 60, 60), speedLevel1));
+            backgroundObjects.Add(new BackgroundObject(backgroundTextures[0], new Rectangle(400, 180, 40, 40), speedLevel2));
+            backgroundObjects.Add(new BackgroundObject(backgroundTextures[0], new Rectangle(600, 100, 30, 30), speedLevel3));
+            backgroundObjects.Add(new BackgroundObject(backgroundTextures[0], new Rectangle(600, 100, 30, 30), speedLevel3));
+
+            //skater
             skater = new Skater(skaterTextures, new Rectangle(_graphics.PreferredBackBufferWidth / 4, 280, 141, 180), boardTextures);
             jumpKeyPressed = false;
 
-            
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            //background textures
+            backgroundTextures.Add(Content.Load<Texture2D>("bush1"));
+            backgroundTextures.Add(Content.Load<Texture2D>("tree"));
 
+            //skater textures
             skaterTextures.Add(Content.Load<Texture2D>("newskaterTexture1"));
             skaterTextures.Add(Content.Load<Texture2D>("newskaterTexture2"));
             skaterTextures.Add(Content.Load<Texture2D>("newUp"));
             skaterTextures.Add(Content.Load<Texture2D>("newDown"));
 
+            //board textures
             boardTextures.Add(Content.Load<Texture2D>("Board Straight")); //-----[0]regular
             boardTextures.Add(Content.Load<Texture2D>("Board Up")); //-----------[1]ollie, front shuv
             boardTextures.Add(Content.Load<Texture2D>("Board Perpendicular")); //[2]back/front shuv
@@ -74,10 +112,52 @@ namespace Trick_tests
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            //check screen change
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+                currentScreen = Screen.MainGame;
 
+            //title update logic
+            if (currentScreen == Screen.Title)
+            {
+                Title();
+            }
+
+            //maingame update logic
+            if (currentScreen == Screen.MainGame)
+            {
+                MainGame(gameTime);
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected void Title()
+        {
+
+        }
+
+        protected void MainGame(GameTime gameTime)
+        {
             keyboardState = Keyboard.GetState();
 
+            //background
+            //foreach (BackgroundObject backgroundObject in backgroundObjects)
+            //    backgroundObject.Move();
+
+
+            //background
+            for (int i = 0; i < backgroundObjects.Count; i++)
+            {
+
+                backgroundObjects[i].Move();
+
+                if (backgroundObjects[i].Bounds.X <= (0 - backgroundObjects[i].Bounds.Width))
+                {
+                    backgroundObjects[i].Bounds = new Rectangle(generator.Next(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferWidth + 100), backgroundObjects[i].Bounds.Y, backgroundObjects[i].Bounds.Width, backgroundObjects[i].Bounds.Height);
+                }
+            }
+
+            //skater
             if (keyboardState.IsKeyDown(Keys.S))
             {
                 if (jumpKeyPressed == false)
@@ -88,10 +168,10 @@ namespace Trick_tests
             }
 
             if (keyboardState.IsKeyUp(Keys.S) && jumpKeyPressed == true)
-			{
+            {
                 skater.Jump(gameTime, jumpStartTime);
                 jumpKeyPressed = false;
-			}
+            }
 
             else if (keyboardState.IsKeyDown(Keys.D))
             {
@@ -121,20 +201,42 @@ namespace Trick_tests
             skater.Trick(trick, gameTime);
             skater.Update(gameTime);
 
-            base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            //draw title elements
+            if (currentScreen == Screen.Title)
+            {
+                GraphicsDevice.Clear(Color.CornflowerBlue);
+                DrawTitle();
+            }
 
-            // TODO: Add your drawing code here
-
-            _spriteBatch.Begin();
-            skater.Draw(_spriteBatch);
-            _spriteBatch.End();
+            //draw main game elements
+            if (currentScreen == Screen.MainGame)
+            {
+                GraphicsDevice.Clear(Color.CornflowerBlue);
+                DrawMainGame();
+            }
 
             base.Draw(gameTime);
         }
+
+        protected void DrawTitle()
+        {
+
+        }
+
+        protected void DrawMainGame()
+        {
+            _spriteBatch.Begin();
+            for (int i = 0; i < backgroundObjects.Count; i++)
+            {
+                backgroundObjects[i].Draw(_spriteBatch);
+            }
+            skater.Draw(_spriteBatch);
+            _spriteBatch.End();
+        }
+
     }
 }
